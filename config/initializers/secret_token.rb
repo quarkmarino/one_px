23068bff6aa39bf4783b6776ac714a13c5721cc9
Mario A Chavez <mario.chavez@gmail.com>
2013-07-31 21:20:55 -0500

    Inicializacion del proyecto de Rails.
    1.- Se agrego el controlador Photos (PhotosController).
    2.- Se agregaron las rutas para el contralador Photos (routes.rb).
    3.- Se crearon las vistas index y new para el PhotosController
    4.- Se creo el modelo Photo y su migracion.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OnePx::Application.config.secret_key_base = 'bb1f143e4e0b32d3dc8c758370682d908bfd1db81e9f72a66c29542fbc9603a14e0f74825a40c846829ce30d2245f819f68ddb31a44773c7c1b3c0ce750a1628'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OnePx::Application.config.session_store :cookie_store, key: '_one_px_session'
